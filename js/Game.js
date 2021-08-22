1ab93b09051460e921be5a5a94dc38472d451d32
nirmit-gangani <nirmitgangani@gmail.com>
2021-08-22 03:30:31 -0700

    test

--- js/Game.js
+++ js/Game.js
@@ -23,4 +23,28 @@ class Game {
       form.display();
     }
   }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers!==undefined){
+      var displayPosition=130;
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index)
+          fill("red");
+        else
+          fill("black");
+        displayPosition+=20; 
+        textSize(15);
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition)
+      }
+      
+    }
+    if(keyIsDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50;
+      player.update();
+    }
+  }
 }
